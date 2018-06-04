983240dc592c482525130ca359da7b9e2e3e43d0
Brian Luciano <blu226@g.uky.edu>
2018-06-04 10:42:54 -0400

    created new metrics file

---
+++ metrics.py
@@ -0,0 +1,46 @@
+from constants import *
+
+def compute_metrics(lines, total_messages, delivery_time):
+    delivered = 0
+    latency = 0
+    energy = 0
+    mes_IDs = []
+
+    for line in lines:
+        line_arr = line.strip().split("\t")
+        if int(line_arr[4]) < delivery_time and int(line_arr[0]) not in mes_IDs:
+            delivered += 1
+            latency += int(line_arr[6])
+            # energy += float(line_arr[7])
+            mes_IDs.append(int(line_arr[0]))
+
+    if delivered > 0:
+        latency = float(latency)/delivered
+        energy = float(energy)/delivered
+
+    if total_messages > 0:
+        delivered = float(delivered) / total_messages
+
+    print("t: ", t, " msg: ", total_messages, " del: ", delivered, "lat: ", latency)
+
+    return delivered, latency, energy
+
+
+#Main starts here
+msg_file = open("generated_messages.txt", "r")
+total_messages = len(msg_file.readlines()[1:])
+
+metric_file = open(Link_Exists_path + metrics_file_name, "w")
+f = open(Link_Exists_path + delivery_file_name, "r")
+
+lines = f.readlines()[2:]
+
+delivery_times = [i for i in range(0, T + 10, 10)]
+
+
+metric_file.write("#t\tPDR\tLatency\tEnergy\n")
+for t in delivery_times:
+    avg_pdr, avg_latency, avg_energy = compute_metrics(lines, total_messages, t)
+    metric_file.write(str(t) + "\t" + str(avg_pdr) + "\t" + str(avg_latency) + "\t" + str(avg_energy) + "\n")
+
+metric_file.close()
